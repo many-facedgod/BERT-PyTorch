2c72b723d8b11b4da180143dcb35c99479543ad3
Karthika <karthika601@gmail.com>
2019-03-18 17:01:25 -0400

    Added feedforward

--- src/utils/TransformerBlock.py
+++ src/utils/TransformerBlock.py
@@ -1,32 +1,28 @@
 from torch import nn
 import torch
 import math
+from LayerNorm import BertLayerNorm
 
 class TransformerBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, config_dict):
         super(TransformerBlock).__init__()
+        self.intermediate_size = config_dict["intermediate_size"]
+        self.hidden_size = config_dict["hidden_size"]
+        self.attention = SelfAttention(config_dict)
+        self.feedforward = FeedForward(config_dict)
+        self.intermediate_linear = nn.Linear(self.hidden_size, self.intermediate_size)
 
     def gelu(self, input):
         return input * 0.5 * (1.0 + torch.erf(input / math.sqrt(2.0)))
 
-    def forward(self):
-        pass
-
-
-class FeedForward(nn.Module):
-    def __init__(self, config_dict):
-        super(FeedForward).__init__()
-        hidden_size = config_dict["hidden_size"]
-        dropout_rate = config_dict["dropout_rate"]
-        self.dropout = nn.Dropout(dropout_rate)
-
-        self.linear = nn.Linear(hidden_size, hidden_size)
-
-
+    def forward(self, input):
+        self_att_output = self.attention(input)
+        intermediate_linear_op = self.gelu(self.intermediate_linear(self_att_output))
+        return self.feedforward(intermediate_linear_op)
 
 class SelfAttention(nn.Module):
     def __init__(self, config_dict):
         super(SelfAttention).__init__()
 
     def forward(self, input):
-        pass
+        return  self.layer_norm(input + self.dropout(self.linear(input)))
