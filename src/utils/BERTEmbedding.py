7cab571cf37895e7380b0b15a5154380da7f091b
Tanmaya Dabral <tanmayadabral@gmail.com>
2019-03-18 16:37:09 -0400

    Minor changes

--- src/utils/BERTEmbedding.py
+++ src/utils/BERTEmbedding.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from LayerNorm import BertLayerNorm
 
 
 class BERTEmbedding(nn.Module):
@@ -17,6 +18,7 @@ class BERTEmbedding(nn.Module):
                                  BERTEmbedding._generate_sinusoid_pos_embedding(config_dict["max_sentence_length"],
                                                                                 config_dict["hidden_size"]))
         self.dropout = nn.Dropout(config_dict["dropout_rate"])
+        self.layer_norm = BertLayerNorm(config_dict)
 
     @staticmethod
     def _generate_sinusoid_pos_embedding(max_len, embedding_dim):
@@ -44,6 +46,7 @@ class BERTEmbedding(nn.Module):
         if self.config_dict["positional_learnt"]:
             positional_embedding = self.positional_embedding[:length].unsqueeze(0).expand_as(sentence_embedding)
         else:
-            positional_embedding = self.positional_embedding(torch.arange(0, length, device=sentences.device)).unsqueeze(0).expand_as(sentence_embedding)
+            positional_embedding = self.positional_embedding(
+                torch.arange(0, length, device=sentences.device)).unsqueeze(0).expand_as(sentence_embedding)
         summed_embedding = token_embedding + positional_embedding + sentence_embedding
-        pass
+        return self.dropout(self.layer_norm(summed_embedding))
